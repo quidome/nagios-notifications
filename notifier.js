e4eea0a4e0e118900d7000b4fbab5623552c7fc9
Quido Meijer <quidome@gmail.com>
2014-01-16 01:20:32 +0100

    with unix line ends now
    supports only one pushbullet message at the time. Should fix this.

--- notifier.js
+++ notifier.js
@@ -42,12 +42,29 @@ var argv = require('optimist')
 */
 
 // Handle input parameters
-var addresses = argv.A.split(";");
+var addresses = argv.A.split(",");
 var host = argv.H;
 var type = argv.T;
 var description = argv.D;
 var state = argv.S;
 var output = argv.O;
+
+
+// prepare the message
+var noteTitle;
+var noteBody;
+if (type == 'host') {
+	noteTitle = 'Host status update';
+	noteBody = host.concat(' changed into state: ', state, ' : ', output);
+} else if (type == 'service') {
+	noteTitle = 'Service status update';
+	noteBody = host.concat(' changed into state: ', state, ' for service: ', description, ': ', output);				
+} else {
+	noteTitle = 'unknown type';
+	noteBody = host.concat(' changed into state: ', state, ' for : ', description, ': ', output);
+}
+// console.log("title: %s", noteTitle);
+// console.log("body: %s", noteBody);
 
 
 // Iterate over addresses
@@ -62,45 +79,19 @@ addresses.map( function(address) {
 		var service = parts[0];
 		var destination = parts[1];
 
-		// console.log("service: %s", service);
-		// console.log("destination: %s", destination);
+		console.log("service: %s", service);
+		console.log("destination: %s", destination);
 
 		if (service == 'pushbullet') {
 			// we've got a winner
-			var s = destination.split(",");
+			var s = destination.split("!");
 			var apikey = s[0];
 			var deviceId = s[1];
-			// console.log("push message to:\nkey: %s\ndevice: %s\n", apikey, deviceId);
 
-			// this script is but-ugly but I have the vars :)
-			var noteTitle;
-			var noteBody;
-			// prepare the message
-			if (type == 'host') {
-				noteTitle = 'Host status update';
-				noteBody = host.concat(' changed into state: ', state, ' : ', output);
-			} else if (type == 'service') {
-				noteTitle = 'Host status update';
-				noteBody = host.concat(' changed into state: ', state, ' for service: ', description, ': ', output);				
-			} else {
-				noteTitle = 'unknown type';
-				noteBody = host.concat(' changed into state: ', state, ' for : ', description, ': ', output);
-			}
-
-			// console.log("title: %s", noteTitle);
-			// console.log("body: %s", noteBody);
+			console.log("apikey: %s\ndevice: %s\n", apikey, deviceId);
 
 			// we should push the message now
 			var pusher = new PushBullet(apikey);
-
-			// // get devices
-			// pusher.devices(function(error, response) {
-			// 	if (error) {
-			// 		console.log("error: %j", error);
-			// 	} else {
-			// 		console.dir(response);
-			// 	}
-			// });
 
 			// push message to given device
 			pusher.note(deviceId, noteTitle, noteBody, function(error, response) {
@@ -110,9 +101,6 @@ addresses.map( function(address) {
 					console.dir(response);
 				}
 			});
-
 		}		
-
 	}
-
 });
